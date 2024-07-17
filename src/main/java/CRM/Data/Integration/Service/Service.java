e98c84b3dcd080810bc8586550ef251d99e679e9
saurabh17kumar <kumar.saurabh@dbalounge.com>
2024-07-17 13:19:54 +0530

    remove space

--- src/main/java/CRM/Data/Integration/Service/Service.java
+++ src/main/java/CRM/Data/Integration/Service/Service.java
@@ -5,7 +5,6 @@ import CRM.Data.Integration.Model.CrmData;
 import java.util.HashMap;
 import java.util.List;
 
-@org.springframework.stereotype.Service
 public interface Service {
 
     CrmData getCustomerData();
